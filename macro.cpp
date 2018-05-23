6b036950b34642e3f50b95f457f8b30b5e1905ec
vitorhpgomes <vitorhpgomes@gmail.com>
2018-05-23 11:10:57 -0300

    substitui a macro por essa aqui

--- macro.cpp
+++ macro.cpp
@@ -2,10 +2,10 @@
 para colocar na MNT(Nao pensei ainda em outra forma melhor de MDT). Ele coloca a linha referente a MDT na MNT. Ainda nao contei
 argumentos nem expandi a macro quando chama.*/
 #include <iostream>
-#include <fstream>                           //Para lidar com o arquivo. Nunca usei isso, ent√£o n√£o sei comofas
+#include <fstream>                           //Para lidar com o arquivo. Nunca usei isso, ent„o n„o sei comofas
 #include <string.h>
 #include <list>
-#include <ctype.h>                          // Usando na fun√ß√£o isalpha
+#include <ctype.h>                          // Usando na funÁ„o isalpha
 #include <sstream>                          // Para retirar as palavras da linha
 #include <algorithm>                        // Para procurar na tabela de simbolos
 #include <string>
@@ -17,6 +17,7 @@ typedef struct MacroNameTable {
     int nargumentos;
     int linhamdt;
     int linhamdtfim;
+    string macroarg[4];
 } MacroNameTable;
 
 
@@ -42,7 +43,7 @@ void printaTabSimTemp (list <MacroNameTable> MNT) {
     it = MNT.begin();
     while (it != MNT.end()) {
         texto = it->label;
-        cout << "Label: " << texto << "\tArgumentos: " << it->nargumentos << "\t LinhaMDT: " <<  it->linhamdt << "\t LinhaMDTFIM: " << it->linhamdtfim << endl;
+        cout << "Label: " << texto << "\tArgumentos: " << it->nargumentos << "\t LinhaMDT: " <<  it->linhamdt << "\t LinhaMDTFIM: " << it->linhamdtfim << "\t MacroArg: " << it->macroarg[0] << "\t MacroArg: " << it->macroarg[1] << "\t MacroArg: " << it->macroarg[2] << "\t MacroArg: " << it->macroarg[3] << endl;
         it++;
     }
 }
@@ -64,7 +65,7 @@ int tabSimSeek (list <MacroNameTable> MNT, string token) {
     }
 }
 
-int tabSimSeek2 (list <MacroNameTable> MNT, string token) {
+int tabSimSeek2 (list <MacroNameTable> MNT, string token,int *nargumentos, int *linhamdt, int *linhamdtfim, string *arg1, string *arg2, string *arg3, string *arg4) {
     list <MacroNameTable> :: iterator it;
 
     it = MNT.begin();
@@ -76,6 +77,13 @@ int tabSimSeek2 (list <MacroNameTable> MNT, string token) {
             it++;
         }
         else {
+            *nargumentos = it->nargumentos;
+            *linhamdt = it->linhamdt;
+            *linhamdtfim = it->linhamdtfim;
+            *arg1 = it->macroarg[0];
+            *arg2 = it->macroarg[1];
+            *arg3 = it->macroarg[2];
+            *arg4 = it->macroarg[3];
             return it->linhamdtfim;
         }
     }
@@ -101,7 +109,7 @@ int tabSimSeek3 (list <MacroNameTable> MNT, string token) {
 void macro (list <MacroNameTable> *MNT, string nome){ //Um tanto desses inteiros eu copiei da sua parte e tenho medo de apagar
     ifstream arquivo;
     ofstream codprep;
-    string token,linha,tokenaux,mdt[100],argmacro,argumentodeclarado[10],argumentochamado[10];
+    string token,linha,tokenaux,mdt[100],argmacro,argumentodeclarado[10],argumentochamado[10],mdtaux;
     int equflag,ifflag,nextlineflag,i=0, endereco=0, op=-1, simEndereco=-1,lala,macroflag=0,argumeto;
     int mdtcont=0,contarg=0,mdtsearch = 0, fim,z,macroflag2,endmacroflag,tirarlinha,mdtfim,trocaargumentos,nargumentos;
     MacroNameTable SimAtual;
@@ -116,9 +124,7 @@ void macro (list <MacroNameTable> *MNT, string nome){ //Um tanto desses inteiros
         stringstream linhaStream(linha);
         while(linhaStream >> token){
             //errotoken
-            if((mdtsearch = tabSimSeek(*MNT,token)) > -1){
-                mdtfim = tabSimSeek2(*MNT,token);
-                nargumentos = tabSimSeek3(*MNT,token);
+            if((tabSimSeek2(*MNT,token,&nargumentos,&mdtsearch,&mdtfim,&argumentodeclarado[0],&argumentodeclarado[1],&argumentodeclarado[2],&argumentodeclarado[3])) > -1){
                 tirarlinha = 1;
                 while(linhaStream >> token){
                     contarg++;
@@ -140,34 +146,36 @@ void macro (list <MacroNameTable> *MNT, string nome){ //Um tanto desses inteiros
                     }
                 }
                 for(mdtsearch;mdtsearch < mdtfim;mdtsearch++){
+                    mdtaux = mdt[mdtsearch];
                     if(trocaargumentos == 1){
-                        if(mdt[mdtsearch].find(argumentodeclarado[0]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[0]),max(argumentochamado[0].length(),argumentodeclarado[0].length()),argumentochamado[0]);
+                        if(mdtaux.find(argumentodeclarado[0]) < 1000){
+                                cout << argumentochamado[0] << endl;
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[0]),max(argumentochamado[0].length(),argumentodeclarado[0].length()),argumentochamado[0]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[1]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[1]),max(argumentochamado[1].length(),argumentodeclarado[1].length()),argumentochamado[1]);
+                        if(mdtaux.find(argumentodeclarado[1]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[1]),max(argumentochamado[1].length(),argumentodeclarado[1].length()),argumentochamado[1]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[2]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[2]),max(argumentochamado[2].length(),argumentodeclarado[2].length()),argumentochamado[2]);
+                        if(mdtaux.find(argumentodeclarado[2]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[2]),max(argumentochamado[2].length(),argumentodeclarado[2].length()),argumentochamado[2]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[3]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[3]),max(argumentochamado[3].length(),argumentodeclarado[3].length()),argumentochamado[3]);
+                        if(mdtaux.find(argumentodeclarado[3]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[3]),max(argumentochamado[3].length(),argumentodeclarado[3].length()),argumentochamado[3]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[0]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[0]),max(argumentochamado[0].length(),argumentodeclarado[0].length()),argumentochamado[0]);
+                        if(mdtaux.find(argumentodeclarado[0]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[0]),max(argumentochamado[0].length(),argumentodeclarado[0].length()),argumentochamado[0]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[1]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[1]),max(argumentochamado[1].length(),argumentodeclarado[1].length()),argumentochamado[1]);
+                        if(mdtaux.find(argumentodeclarado[1]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[1]),max(argumentochamado[1].length(),argumentodeclarado[1].length()),argumentochamado[1]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[2]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[2]),max(argumentochamado[2].length(),argumentodeclarado[2].length()),argumentochamado[2]);
+                        if(mdtaux.find(argumentodeclarado[2]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[2]),max(argumentochamado[2].length(),argumentodeclarado[2].length()),argumentochamado[2]);
                         }
-                        if(mdt[mdtsearch].find(argumentodeclarado[3]) < 1000){
-                                mdt[mdtsearch].replace(mdt[mdtsearch].find(argumentodeclarado[3]),max(argumentochamado[3].length(),argumentodeclarado[3].length()),argumentochamado[3]);
+                        if(mdtaux.find(argumentodeclarado[3]) < 1000){
+                                mdtaux.replace(mdtaux.find(argumentodeclarado[3]),max(argumentochamado[3].length(),argumentodeclarado[3].length()),argumentochamado[3]);
                         }
                     }
 
-                    codprep << mdt[mdtsearch] << endl;
+                    codprep << mdtaux << endl;
                 }
             }
             if((iequals(token,"MACRO") == 1)){//Verifica se o token e um EQU, se for EQU ele manda o label anterior pra tabela
@@ -175,7 +183,7 @@ void macro (list <MacroNameTable> *MNT, string nome){ //Um tanto desses inteiros
                 macroflag2 = 1;
                 if ((tokenaux.back()) == ':') {//Verifica se o token anterior a macro e um label
                     tokenaux.pop_back();
-                    simEndereco = tabSimSeek(*MNT, tokenaux);
+                    simEndereco = tabSimSeek2(*MNT, tokenaux,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
                     if (simEndereco > 0) {
                         cout << "Erro, simbolo ja definido: |" << tokenaux << endl;
                     }
@@ -197,7 +205,7 @@ void macro (list <MacroNameTable> *MNT, string nome){ //Um tanto desses inteiros
                             if (argmacro.front() != '&'){
                                 cout << "Argumento na forma invalida" << endl;
                             }else{
-                                argumentodeclarado[z] = argmacro;
+                                SimAtual.macroarg[z] = argmacro;
                                 z++;
                             }
                         }
